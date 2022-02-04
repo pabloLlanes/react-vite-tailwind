4ff1ab5ceacb51f94281d04d31ea68efb646113a
Pablo Llanes <pablojllanes@gmail.com>
2022-02-03 22:18:42 -0300

    initials configurations react vite

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <section class='bg-gradient-to-l from-blue-400  to-gray-700 flex items-end '>
+        <div class='container mx-auto flex md:flex-row flex-col h-full text-white'>
+          <div class='lg:flex-grow p-4  mb-16 md:mb-0 items-center text-center'>
+            <h1 class=' sm:text-4xl text-3xl mb-4 font-medium '>
+              REACT JS
+            </h1>
+            <p class='mb-2 leading-relaxed'>
+              Template with React, Vite y Tailwind css
+              
+            </p>
+         
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
